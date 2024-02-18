3c2d5ea4bbe8112bc209b3971b60619c1ceefdce
CaioBalc <balczarekcaio@gmail.com>
2024-02-18 20:42:06 -0300

    MinhaClasseAvançado

--- src/MinhaClasseAvancado.php
+++ src/MinhaClasseAvancado.php
@@ -1,8 +1,25 @@
 <?php
 
 class MinhaClasseAvancado {
-    function somar($a, $b){
-        return $a + $b;
+    function somar(array $lista): int{
+        foreach ($lista as $elemento) {
+            if (!is_numeric($elemento)) {
+                return 0;
+            }
+        }
+        return array_sum($lista);
+    }
+
+    function subtrair($a, $b, /*array $lista*/){
+        /*
+        return array_reduce(array_slice($lista, 1), function ($resultado, $elemento) {
+            return $resultado - $elemento;
+        }, reset($lista));
+        */
+        if (!is_numeric($a) ||!is_numeric($b) ) {
+            return 0;
+        }
+        return $a - $b;
     }
 }
 
--- tests/MinhaClasseAvancadoTest.php
+++ tests/MinhaClasseAvancadoTest.php
@@ -5,19 +5,44 @@ use PHPUnit\Framework\TestCase;
 require_once "src/MinhaClasseAvancado.php";
 
 class MinhaClasseAvancadoTest extends TestCase{
-    public function testSoma(){
-        $classe = new MinhaClasseAvancado();
+    public function testSomar(){
+        $lista = new MinhaClasseAvancado;
         # Valores positivos
-        $resultado = $classe->somar(2, 3);
-        $this->assertEquals(5, $resultado);
+        $this->assertEquals(5, $lista->somar([2, 3]));
         # Valores negativos
-        $resultado = $classe->somar(-2, -3);
-        $this->assertEquals(-5, $resultado);
+        $this->assertEquals(-5, $lista->somar([-2, -3]));
         # Valores diferentes
-        $resultado = $classe->somar(-2, 3);
-        $this->assertEquals(1, $resultado);
+        $this->assertEquals(1, $lista->somar([-2, 3]));
         # Adição de zero
-        $resultado = $classe->somar(0, 3);
-        $this->assertEquals(3, $resultado);
+        $this->assertEquals(3, $lista->somar([0, 3]));
+        # Valores decimais
+        $this->assertEquals(15, $lista->somar([10, 33, -28]));
+        # Valores não numérico
+        $this->assertEquals(0, $lista->somar(["Frase tal", "Palavra"]));
+    }
+
+    public function testSubtrair(){
+        $lista = new MinhaClasseAvancado;
+        /*
+        # Valores positivos
+        $this->assertEquals(1, $lista->subtrair([3, 2]));
+        */
+        # Valores positivos
+        $resultado = $lista->subtrair(2, 3);
+        $this->assertEquals(-1, $resultado);
+        # Valores negativos
+        $resultado = $lista->subtrair(-2, -3);
+        $this->assertEquals(1, $resultado);
+        # Valores diferentes
+        $resultado = $lista->subtrair(-2, 3);
+        $this->assertEquals(-5, $resultado);
+        # Adição de zero
+        $resultado = $lista->subtrair(0, 2);
+        $this->assertEquals(-2, $resultado);
+        # Valores decimais
+        $resultado = $lista->subtrair(33, 22);
+        $this->assertEquals(11, $resultado);
+        # Valores não numérico
+        $this->assertEquals(0, $lista->somar(["Frase tal", "Palavra"]));
     }
 }
